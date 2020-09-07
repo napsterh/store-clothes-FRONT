01d946dec3c349c4f7bc11ca98d7bf393edff29d
Napster <rome.1205h@gmail.com>
2020-09-07 03:29:42 -0500

    primer commit de front

---
+++ src/components/Navbar.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import logo from '../assets/laganga.png'
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light p-1">
+        <Link className="navbar-brand" to="/products">
+            <img src={logo} alt="La Ganga Logo" width="150"/>
+        </Link>
+        <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+        >
+            <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ml-auto">
+            <li className="nav-item active">
+                <Link className="nav-link" to="/products">
+                    Inicio
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link" to="/products">
+                    Productos
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link" to="/products-form">
+                    Crear productos
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link" to="/products-form">
+                    Iniciar sesión
+                </Link>
+            </li>
+            </ul>
+        </div>
+        </nav>
+    );
+}
+
+export default Navbar;
---
+++ src/components/ProductForm.js
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import axios from 'axios';
+
+const API = process.env.REACT_APP_API;
+
+const ProductForm = () => {
+  
+    const [ state, setState ] = useState({
+        name: "",
+        price: "",
+        description: "",
+        previewImageURL: ""
+    })
+  
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const res =  await axios.post(`${API}/products`, state)
+        console.log(res);
+  };
+
+  const handleInputChange = (e) => {
+    setState({
+        ...state,
+        [e.target.name]: e.target.value,
+    })
+  }
+
+  return (
+    <div className="row">
+      <div className="col-md-4 offset-md-4">
+        <div className="card card-body bg-dark">
+          <h3 className="text-white">Crear un nuevo producto</h3>
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+                <input
+                    type="text"
+                    name="name"
+                    className="form-control"
+                    placeholder="Agregar producto"
+                    onChange={handleInputChange}
+                    autoFocus
+                />
+                </div>
+            <div className="form-group">
+              <input
+                type="text"
+                name="previewImageURL"
+                className="form-control"
+                placeholder="URL de Imagen"
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="text"
+                name="price"
+                className="form-control"
+                placeholder="Precio"
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="form-group">
+              <textarea
+                name="description"
+                rows="3"
+                className="form-control"
+                placeholder="Agregar una descripcion"
+                onChange={handleInputChange}
+                ></textarea>
+            </div>
+            <button className="btn btn-success btn-block mt-2">Agregar</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductForm;
